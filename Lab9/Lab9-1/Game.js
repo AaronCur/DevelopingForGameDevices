8aed50b4d3239a827d2f8c00185e5049e99355a7
AaronCur <c00202505@itcarlow.ie>
2017-11-07 09:10:23 +0000

    Lab 9-1 complete (Setup Project)

---
+++ Lab9/Lab9-1/Game.js
@@ -0,0 +1,30 @@
+/**
+*helper funtion that constructs game
+*/
+class Game
+{
+  constructor()
+  {
+
+    console.log("game constructed");
+
+
+  }
+
+  initWorld()
+  {
+      console.log("game initialising");
+
+  }
+
+  update()
+  {
+    window.requestAnimationFrame(gameNs.game.update);
+  }
+
+  draw()
+  {
+  
+  }
+
+}
