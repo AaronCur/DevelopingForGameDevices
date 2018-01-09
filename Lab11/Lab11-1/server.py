81eb666cdac79bc3e6c510092eb95c7697ad31f8
AaronCur <c00202505@itcarlow.ie>
2018-01-09 14:47:05 +0000

    Lab 11 part 1 complete

--- Lab11/Lab11-1/server.py
+++ Lab11/Lab11-1/server.py
@@ -8,10 +8,11 @@ class WSHandler(tornado.websocket.WebSocketHandler):
 
         	def open(self):
                     	pass
+                        print("Connection opened")
 
         	def on_message(self, message):
-                self.write_message("You said: " + message)
                     	pass
+                        self.write_message("You said : " + message)
 
         	def on_close(self):
         	    pass
