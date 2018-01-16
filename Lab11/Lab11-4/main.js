b48cd772a203d28064f5c508362b533e43a134b2
AaronCur <c00202505@itcarlow.ie>
2018-01-16 09:27:50 +0000

    Lab 11 complete #time 2h 30m

---
+++ Lab11/Lab11-4/main.js
@@ -0,0 +1,12 @@
+function main()
+{
+  var ws = new WebSocket("ws://localhost:8080/wstest");
+  //called when the websocket is opened
+  ws.onopen = function() {
+     ws.send("Hello, world");
+  };
+  //called when the client receives a message
+  ws.onmessage = function (evt) {
+     alert(evt.data);
+  };
+}
---
+++ Lab11/Lab11-4/server.py
@@ -0,0 +1,35 @@
+from tornado import websocket, web, ioloop, httpserver
+import tornado
+
+session ={}
+
+class WSHandler(tornado.websocket.WebSocketHandler):
+
+             def check_origin(self, origin):
+                return True
+
+             def open(self):
+                pass
+                print("Connection opened")
+
+             def on_message(self, message):
+                pass
+                self.write_message("You said: " + message)
+                print(self.request.remote_ip)
+                print(self.stream.socket.getpeername()[1])
+                player_address =str(self.request.remote_ip) + ":" + str(self.stream.socket.getpeername()[1])
+                session[player_address] = self
+                print(session)
+
+             def on_close(self):
+                pass
+
+app= tornado.web.Application([
+        	#map the handler to the URI named "test"
+        	(r'/wstest', WSHandler),
+])
+
+if __name__ == '__main__':
+        	server_port=8080
+        	app.listen(server_port)
+        	ioloop.IOLoop.instance().start()
